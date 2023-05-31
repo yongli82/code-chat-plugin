3dc00164751471cc0ca20f949be6455d8660b585
GitHub Action <action@github.com>
2023-05-31 09:35:41 +0000

    Template cleanup

--- src/main/kotlin/com/github/yongli82/codechatplugin/listeners/MyFrameStateListener.kt
+++ src/main/kotlin/com/github/yongli82/codechatplugin/listeners/MyFrameStateListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.yongli82.codechatplugin.listeners
 
 import com.intellij.ide.FrameStateListener
 import com.intellij.openapi.diagnostic.thisLogger
--- src/main/kotlin/com/github/yongli82/codechatplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/yongli82/codechatplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.yongli82.codechatplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yongli82.codechatplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
